08319098907a9dba4d8d2a29a6571b7e0a11f35f
blueror815 <blueror815@hotmail.com>
2016-03-25 16:14:32 +0100

    change db setting for production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/angle',
+	db: 'mongodb://TravisUser:TravisUser@ec2-54-144-250-149.compute-1.amazonaws.com:27017/travis-meanDB',
 	assets: {
 		lib: {
 			css: [
